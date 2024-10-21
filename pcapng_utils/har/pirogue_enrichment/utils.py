05555a7e7202a3eae39114e76794b808ff170a92
Etienne Maheux <etienne.maheux@peren.gouv.fr>
2024-10-21 19:16:34 +0200

    Efficient socket trace <-> network traffic pairing

--- pcapng_utils/har/pirogue_enrichment/utils.py
+++ pcapng_utils/har/pirogue_enrichment/utils.py
@@ -1,13 +1,14 @@
 import base64
 from re import sub
+from typing import TypeVar
 
 
-def to_camel_case(s):
+def to_camel_case(s: str) -> str:
     s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
     return "".join([s[0].lower(), s[1:]])
 
 
-def prefix_string_camel_case(key: str, prefix: str = "_"):
+def _to_camel_case_after_prefix(key: str, prefix: str) -> str:
     return f"{prefix}{to_camel_case(key)}"
 
 
@@ -17,13 +18,21 @@ def clean_prefixed_ip_address(ip_address: str) -> str:
     return ip_address
 
 
-def base64_to_hex(base64_encoded_data: str, validate: bool = True) -> str:
+def base64_to_hex(base64_encoded_data: str, *, validate: bool = True) -> str:
     """Convert a base64 encoded string to a hexadecimal string"""
-    if not base64_encoded_data:
-        return ''
-    # Try to decode the base64 encoded data
-    try:
-        binary_payload = base64.b64decode(base64_encoded_data, validate=validate)
-        return binary_payload.hex()
-    except Exception:
-        return ''
+    return base64.b64decode(base64_encoded_data, validate=validate).hex()
+
+
+_T = TypeVar('_T', dict, list, str)
+
+
+def keys_to_camel_case(obj: _T, *, prefix: str = '') -> _T:
+    """Recursively rename all keys of dictionaries within object with camel case (optionally prefixed)."""
+    if isinstance(obj, dict):
+        return {
+            _to_camel_case_after_prefix(k, prefix): keys_to_camel_case(v, prefix=prefix)
+            for k, v in obj.items()
+        }
+    if isinstance(obj, list):
+        return [keys_to_camel_case(k, prefix=prefix) for k in obj]
+    return obj
